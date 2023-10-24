2f0089c63452a030cf7cdfb45301ae3bff696e26
Otabek Tuychiev <69725678+Denis13tm@users.noreply.github.com>
2023-10-24 10:34:21 +0900

    Updated  some code parts

--- ROS_Smart_Mobility_Course_Collection_of_Activitis/Week_7/SM_action_image_processor_server.cpp
+++ ROS_Smart_Mobility_Course_Collection_of_Activitis/Week_7/SM_action_image_processor_server.cpp
@@ -1,50 +1,52 @@
 #include <rclcpp/rclcpp.hpp>
 #include <example_interfaces/action/image_processing.hpp>
 
-class ImageProcessorServer : public rclcpp::Node
+class ImageProcessingServerNode : public rclcpp::Node
 {
 public:
-    explicit ImageProcessorServer()
-        : Node("image_processor_server")
+    explicit ImageProcessingServerNode()
+        : Node("image_processing_server_node")
     {
-        action_server_ = create_server<example_interfaces::action::ImageProcessing>(
-            "image_processing",
-            std::bind(&ImageProcessorServer::handle_goal, this, std::placeholders::_1, std::placeholders::_2),
-            std::bind(&ImageProcessorServer::handle_cancel, this, std::placeholders::_1),
-            std::bind(&ImageProcessorServer::handle_accepted, this, std::placeholders::_1));
+        image_processing_action_server_ = create_server<example_interfaces::action::ImageProcessing>(
+            "image_processing_action",
+            std::bind(&ImageProcessingServerNode::process_image_goal, this, std::placeholders::_1, std::placeholders::_2),
+            std::bind(&ImageProcessingServerNode::cancel_image_processing, this, std::placeholders::_1),
+            std::bind(&ImageProcessingServerNode::handle_accepted_goal, this, std::placeholders::_1));
     }
 
 private:
-    rclcpp::Server<example_interfaces::action::ImageProcessing>::SharedPtr action_server_;
+    rclcpp::Server<example_interfaces::action::ImageProcessing>::SharedPtr image_processing_action_server_;
 
-    void handle_goal(
+    void process_image_goal(
         const rclcpp_action::GoalUUID &,
-        std::shared_ptr<const example_interfaces::action::ImageProcessing::Goal> goal)
+        std::shared_ptr<const example_interfaces::action::ImageProcessing::Goal> image_goal)
     {
-        // Process the image (edge detection)
         // Implement image processing logic here
+        // Process the image (e.g., edge detection)
 
         // Publish result
-        auto result = std::make_shared<example_interfaces::action::ImageProcessing::Result>();
-        action_server_->succeeded(goal, result);
+        auto image_result = std::make_shared<example_interfaces::action::ImageProcessing::Result>();
+        image_processing_action_server_->succeeded(image_goal, image_result);
+
+        RCLCPP_INFO(get_logger(), "Image processing goal succeeded.");
     }
 
-    void handle_cancel(const rclcpp_action::GoalUUID &)
+    void cancel_image_processing(const rclcpp_action::GoalUUID &)
     {
-        RCLCPP_INFO(get_logger(), "Goal was canceled.");
+        RCLCPP_INFO(get_logger(), "Image processing goal was canceled.");
     }
 
-    void handle_accepted(const std::shared_ptr<rclcpp_action::ServerGoalHandle<example_interfaces::action::ImageProcessing>> goal_handle)
+    void handle_accepted_goal(const std::shared_ptr<rclcpp_action::ServerGoalHandle<example_interfaces::action::ImageProcessing>> goal_handle)
     {
         // Goal accepted callback
-        RCLCPP_INFO(get_logger(), "Goal accepted.");
+        RCLCPP_INFO(get_logger(), "Image processing goal accepted.");
     }
 };
 
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<ImageProcessorServer>());
+    rclcpp::spin(std::make_shared<ImageProcessingServerNode>());
     rclcpp::shutdown();
     return 0;
 }
